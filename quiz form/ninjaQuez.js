8324ae4ccaa97ac605ca16eb2407b27c9e6a7449
mahdilogist <106011079+mahdilogist@users.noreply.github.com>
2023-08-13 01:48:58 +0330

    add quiz form to todo list repo

---
+++ quiz form/ninjaQuez.js
@@ -0,0 +1,27 @@
+const correctAnswer = ["B", "B", "B", "B"];
+const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let score = 0;
+    const userAnswer = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+    // check answer
+    userAnswer.forEach((answer, index) => {
+        if (answer === correctAnswer[index]){
+            score += 25;
+        }
+    })
+
+    // show result on page
+    scrollTo(0,0);
+    result.classList.remove('d-none');
+    let output =0;
+    const timer = setInterval(() => {
+        result.querySelector('span').textContent = `${output}%`;
+        if( output == score){
+            clearInterval(timer);
+        } else {
+            output += 1;
+        }
+    }, 20)
+});
